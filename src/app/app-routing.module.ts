c352ea09e3adcadd548be173198f02821ada2ae3
Ephraim Bundi <ebundi04@gmail.com>
2021-08-30 10:52:16 +0300

    Modify app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalComponent } from './personal/personal.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'personal', component: PersonalComponent},
+  { path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
