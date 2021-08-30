6a366ab882d382b51c59b6f845468aec6e31cbfa
Ephraim Bundi <ebundi04@gmail.com>
2021-08-30 09:25:37 +0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PersonalComponent } from './personal/personal.component';
+import { CapsPipe } from './caps.pipe'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    PersonalComponent,
+    CapsPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ ProfileService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
